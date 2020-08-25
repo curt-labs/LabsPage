154af82614acdf53888e88a20f9fbfe9bdb9a941
David Vaini <davidvaini@gmail.com>
2020-08-25 14:25:31 -0500

    Scraped website and added assets

---
+++ Scripts/admin/profile.js
@@ -0,0 +1,33 @@
+﻿// Javascript for profile pages
+
+$(document).ready(function () {
+    // when a user clicks on the api key, we want to tell the browser to select the text of the element
+    $('#APIhighlight').click(
+      function () {
+          SelectText('APIhighlight'); // pass in the element that will be selected
+      }
+    );
+
+
+    // figures out the range of the text in the element and selects all the text within the range.
+    function SelectText(element) {
+        var text = document.getElementById(element);
+        if ($.browser.msie) {
+            var range = document.body.createTextRange();
+            range.moveToElementText(text);
+            range.select();
+        } else if ($.browser.mozilla || $.browser.opera) {
+            var selection = window.getSelection();
+            var range = document.createRange();
+            range.selectNodeContents(text);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        } else if ($.browser.safari) {
+            var selection = window.getSelection();
+            selection.setBaseAndExtent(text, 0, text, 1);
+        } else {
+            // browser is not supported and simply do not include extra functionality.
+        }
+    }
+
+});
---
+++ Scripts/admin/validate.js
@@ -0,0 +1,32 @@
+﻿/*
+Custom Validation using jQuery Validation Plugin
+
+
+*/
+$(document).ready(function () {
+
+    $(".profileForm").validate({
+        rules: {
+            name: { required: true, minlength: 1, maxlength: 50 },
+            customerID: { required: true, number: true, minlength: 1, maxlength: 20 },
+            email: { required: true, email:true, minlength: 1, maxlength: 50 },
+            confirmEmail: { equalTo: "#email", required: true, email:true, minlength: 1, maxlength: 50 },
+            password: { required: true, minlength: 1, maxlength: 30 },
+            oldPassword: { required: true, minlength: 1, maxlength: 30 },
+            newPassword: { required: true, minlength: 1, maxlength: 30 },
+            confirmPassword:{equalTo: "#password", required: true, minlength: 1, maxlength: 30 },
+            confirmNewPassword: { equalTo: "#newPassword", required: true, minlength: 1, maxlength: 30 },
+            username: { required: true, email: true, minlength: 1, maxlength: 50 },
+
+        },
+        errorContainer: ".validationSummary",
+        errorLabelContainer: ".validationSummary ul",
+        wrapper: "li",
+        errorPlacement: function(error, element){
+            error.insertAfter(element.parent()).animate({opacity: .1},1);
+            error.insertAfter(element.parent()).animate({opacity: 1},1200);
+        }
+
+    });
+});
+
